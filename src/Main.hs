df5724a21d63615513ce8fd26c6bf4e2657cae92
Rosario Pulella <RosarioPulella@gmail.com>
2025-05-12 18:22:49 -0400

    Input: Add View

--- src/Main.hs
+++ src/Main.hs
@@ -4,11 +4,13 @@ import System.IO (hFlush, stdout)
 import Data.List (stripPrefix)
 import Control.Monad.Trans.Class (lift)
 import GHC.List (List)
-import Control.Monad.Trans.State (StateT, evalStateT, modify)
+import Control.Monad.Trans.State (StateT, evalStateT, modify, get)
+import Data.Foldable (traverse_)
 
 data Input
   = Exit
   | Add String
+  | View
   | Invaid String
 
 main :: IO ()
@@ -24,17 +26,29 @@ loop = do
     Exit -> do
       lift $ putStrLn "Goodbye!"
       pure ()
-    Add task -> do
-      lift $ putStrLn $ "Task \"" ++ task ++ "\" added to the to-do list."
-      modify (task :)
-      loop
-    Invaid invalidInput -> do
-      lift $ putStrLn $ "Invaid invalidInput: " ++ invalidInput
-      lift $ putStrLn "Please provide valid input."
-      loop
+    _ -> case input of
+      Add task -> do
+        lift $ putStrLn $ "Task \"" ++ task ++ "\" added to the to-do list."
+        modify (task :)
+        loop
+      View -> do
+        s <- get
+        lift $ if null s
+          then putStrLn "No tasks in to-do list"
+          else do
+            putStrLn "Tasks..."
+            traverse_
+              (\(i, t) -> putStrLn $ show i ++ ": " ++ t)
+              $ zip ([1..] :: List Integer) s
+        loop
+      Invaid invalidInput -> do
+        lift $ putStrLn $ "Invaid invalidInput: " ++ invalidInput
+        lift $ putStrLn "Please provide valid input."
+        loop
 
 parseInput :: String -> Input
 parseInput "exit" = Exit
+parseInput "list" = View
 parseInput input = case stripPrefix "add " input of
   Just v -> Add v
   Nothing -> Invaid input
