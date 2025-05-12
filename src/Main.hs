5a84b668a5e8f1fb7a065c7e038a05d4d3a92dc6
Rosario Pulella <RosarioPulella@gmail.com>
2025-05-12 15:53:43 -0400

    Input: Add Add

--- src/Main.hs
+++ src/Main.hs
@@ -1,29 +1,40 @@
 module Main where
 
 import System.IO (hFlush, stdout)
+import GHC.List (List)
+import Data.List (stripPrefix)
 
 data Input
   = Exit
+  | Add String
   | Invaid String
 
 main :: IO ()
 main = do
   putStrLn "Welcome to my TODO List Manager!"
-  loop
+  _ <- loop []
+  pure ()
 
-loop :: IO ()
-loop = do
+loop :: List String ->  IO (List String)
+loop s = do
   input <- parseInput <$> getInput
   case input of
-    Exit -> putStrLn "Goodbye!"
+    Exit -> do
+      putStrLn "Goodbye!"
+      pure s
+    Add task -> do
+      putStrLn $ "Task \"" ++ task ++ "\" added to the to-do list."
+      loop $ task : s
     Invaid invalidInput -> do
       putStrLn $ "Invaid invalidInput: " ++ invalidInput
       putStrLn "Please provide valid input."
-      loop
+      loop s
 
 parseInput :: String -> Input
 parseInput "exit" = Exit
-parseInput input = Invaid input
+parseInput input = case stripPrefix "add " input of
+  Just v -> Add v
+  Nothing -> Invaid input
 
 getInput :: IO String
 getInput = do
