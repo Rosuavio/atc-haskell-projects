a6dbc7bef79e148a699bb011fadc41253c60db54
Rosario Pulella <RosarioPulella@gmail.com>
2025-06-14 19:50:43 -0400

    getDefaultFile

--- src/Main.hs
+++ src/Main.hs
@@ -4,16 +4,16 @@ import Control.Monad (when)
 import qualified Data.Text.IO as T
 import System.Directory.OsPath
   ( Permissions (readable)
-  , XdgDirectory (XdgState)
   , doesFileExist
   , getPermissions
-  , getXdgDirectory
   )
-import System.OsPath (decodeFS, unsafeEncodeUtf)
+import System.OsPath (decodeFS)
+
+import Util
 
 main :: IO ()
 main = do
-  todoFilePath <- getXdgDirectory XdgState $ unsafeEncodeUtf "todo"
+  todoFilePath <- getDefaultFile
   canRead <- doesFileExist todoFilePath >>= \case
     False -> pure False
     True -> readable <$> getPermissions todoFilePath
---
+++ src/Util.hs
@@ -0,0 +1,7 @@
+module Util where
+
+import System.Directory.OsPath (XdgDirectory (XdgState), getXdgDirectory)
+import System.OsPath (OsPath, unsafeEncodeUtf)
+
+getDefaultFile :: IO OsPath
+getDefaultFile = getXdgDirectory XdgState $ unsafeEncodeUtf "todo"
