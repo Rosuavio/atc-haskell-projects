802b3eb711765a679012d102d2bd568bb73799fa
Rosario Pulella <RosarioPulella@gmail.com>
2025-06-14 19:50:43 -0400

    hGetNlines: Return `Seq Text` instead of `[Text]`

--- src/Util.hs
+++ src/Util.hs
@@ -1,15 +1,16 @@
 module Util where
 
+import Data.Sequence (Seq ((:|>)))
 import Data.Text (Text)
 import qualified Data.Text.IO as T
 import System.Directory.OsPath (XdgDirectory (XdgState), getXdgDirectory)
 import System.IO (Handle, hIsEOF)
 import System.OsPath (OsPath, unsafeEncodeUtf)
 
-hGetNLines :: Handle -> Int -> IO [Text]
+hGetNLines :: Handle -> Int -> IO (Seq Text)
 hGetNLines fileHandle = go mempty
   where
-    go :: [Text] -> Int -> IO [Text]
+    go :: Seq Text -> Int -> IO (Seq Text)
     go acc 0 = pure acc
     go acc linesToGet = do
       isEOF <- hIsEOF fileHandle
@@ -17,7 +18,7 @@ hGetNLines fileHandle = go mempty
         True -> pure acc
         False -> do
           line <- T.hGetLine fileHandle
-          go (acc <> [line]) (linesToGet - 1)
+          go (acc :|> line) (linesToGet - 1)
 
 getDefaultFile :: IO OsPath
 getDefaultFile = getXdgDirectory XdgState $ unsafeEncodeUtf "todo"
